491b38f77470395aeab1dbe54396a9433cff9a20
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-05-21 19:17:47 -0300

    Sobre Nosotros Mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./layout/Header";
+import SobreNosotros from "./layout/SobreNosotros";
 import "./main.css"
 
 function App() {
   return (
     <>
       <Header />
+      <SobreNosotros />
     </>
   )
 }
---
+++ src/layout/SobreNosotros.jsx
@@ -0,0 +1,16 @@
+const SobreNosotros = () => {
+    return (
+        <article className="nosotros-container">
+
+            <h2 className="nosotros-heading">Sobre Nosotros</h2>
+            <div className="underline-corte"></div>
+
+            <p className="nosotros-descripcion">Con dos décadas de experiencia en el mercado, Transportes Martínez es una empresa argentina especializada en el transporte de cargas generales mediante camiones, ofreciendo servicios tanto a nivel nacional como internacional. Desde nuestra fundación hace 20 años, nos hemos comprometido a proporcionar soluciones logísticas confiables y eficientes para nuestros clientes.
+                <span>
+                    Contamos con una flota moderna de camiones equipados con tecnología de vanguardia, lo que nos permite adaptarnos a las necesidades específicas de cada cliente y garantizar el cuidado óptimo de la mercancía durante todo el trayecto.</span>
+            </p>
+        </article>
+    )
+}
+
+export default SobreNosotros
